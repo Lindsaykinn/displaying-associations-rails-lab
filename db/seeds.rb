9480d2a3c59ba79b55786c8025ae49a1d2d3878e
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-20 16:30:49 -0500

    commit tree c9cfbe2c45b664c2ec15e00431947ca9226e0962

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artists = Artist.create([
+  {name: "Madonna"},
+  {name: "Elvis Costello"},
+  {name: "Hall & Oates"},
+  {name: "Cardi B"},
+  {name: "Taylor Swift"},
+])
+
+songs = Song.create([
+  {title: "Like a Virgin", artist_id: 1},
+  {title: "Dress You Up", artist_id: 1},
+  {title: "Allison", artist_id: 2},
+  {title: "Your Kiss is on my List", artist_id: 3},
+  {title: "I Like it Like That", artist_id: 4},
+  {title: "Red", artist_id: 5},
+  {title: "Shake it Off", artist_id: 5},
+])
